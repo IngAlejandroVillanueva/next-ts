cde5d1a83c5588deae02fa5ede5a17b4609e55cc
Alejandro Villanueva <ing.alejandrovf@gmail.com>
2022-07-05 15:20:19 -0500

    eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/assets/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@/assets/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
